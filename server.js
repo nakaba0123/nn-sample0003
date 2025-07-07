fbbc1d2967c953ca8d0ffde2340a75be37f0b9f2
Nakaba NAGANO <nakabanagano@NakabanoMacBook-Pro.local>
2025-07-07 16:07:31 +0900

    Fix2

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
